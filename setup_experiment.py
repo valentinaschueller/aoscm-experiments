6f740621989552e7be9ec31d8cb87480197d3f72
Valentina Schüller <valentina.schuller@math.lu.se>
2024-09-03 11:33:42 +0200

    Remove unnecessary function

--- setup_experiment.py
+++ setup_experiment.py
@@ -5,39 +5,6 @@ from context import Context
 from utils.helpers import compute_nstrtini
 
 
-def set_experiment_date_properties(
-    experiment: dict,
-    start_date: pd.Timestamp,
-    simulation_duration: pd.Timedelta,
-    input_file_start_date: pd.Timestamp,
-    input_file_freq: pd.Timedelta,
-) -> None:
-    """set keys in the experiment dictionary that are related to start and end dates of the simulation:
-    - `run_start_date`/`run_end_date`
-    - `ifs_nstrtini`
-
-    :param experiment: experiment dictionary
-    :type experiment: dict
-    :param start_date: run start date
-    :type start_date: pd.Timestamp
-    :param simulation_duration: simulation duration
-    :type simulation_duration: pd.Timedelta
-    :param input_file_start_date: start date of the OpenIFS input (forcing) file
-    :type input_file_start_date: pd.Timestamp
-    :param input_file_freq: frequency of data in the OpenIFS input (forcing) file
-    :type input_file_freq: pd.Timedelta
-    """
-    experiment["run_start_date"] = start_date
-
-    end_date = start_date + simulation_duration
-    experiment["run_end_date"] = end_date
-
-    nstrtini = compute_nstrtini(
-        start_date, input_file_start_date, int(input_file_freq.seconds / 3600)
-    )
-    experiment["ifs_nstrtini"] = nstrtini
-
-
 def set_experiment_input_files(
     experiment: dict,
     context: Context,
