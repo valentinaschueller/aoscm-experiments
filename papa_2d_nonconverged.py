126b5405b5b101e1c98bb967e63c3cb086455112
Valentina Schüller <valentina.schuller@math.lu.se>
2025-03-10 08:41:13 +0100

    Update nonconverged experiment script

--- papa_2d_nonconverged.py
+++ papa_2d_nonconverged.py
@@ -14,13 +14,13 @@ from helpers import AOSCMVersion, get_context
 
 
 def get_nemo_file(data_dir: Path, start_date: pd.Timestamp):
-    return data_dir / "nemo_from_CMEMS" / f"init_PAPASTATION_{start_date.date()}.nc"
+    return data_dir / "nemo_from_CMEMS" / f"nemo_restart_{start_date.date()}.nc"
 
 
 def get_rstas_file(data_dir: Path, start_date: pd.Timestamp):
     date = start_date.date()
     hour = f"{start_date.time().hour:02}"
-    rstas_name = f"rstas_{date}_{hour}_era.nc"
+    rstas_name = f"rstas_{date}_{hour}.nc"
     return data_dir / "rstas_from_AMIP" / rstas_name
 
 
@@ -29,46 +29,46 @@ def get_rstos_file(data_dir: Path, start_date: pd.Timestamp):
 
 
 model_version = AOSCMVersion.ECE3
-mass_flux = True
-context = get_context(model_version, "nwp")
+mass_flux = False
+context = get_context(model_version, "papa")
 
 if model_version == AOSCMVersion.ECE3:
+    ensemble_directory = context.output_dir / "ensemble_output_nc"
     start_dates = [
         pd.Timestamp("2014-07-03 00:00:00"),
         pd.Timestamp("2014-07-12 18:00:00"),
-        pd.Timestamp("2014-07-25 12:00:00"),
     ]
 elif model_version == AOSCMVersion.ECE43:
+    ensemble_directory = context.output_dir / "ensemble_output_43_nc"
     start_dates = [
-        pd.Timestamp("2014-07-19 00:00"),
-        pd.Timestamp("2014-07-07 06:00"),
-        pd.Timestamp("2014-07-10 18:00"),
-        pd.Timestamp("2014-07-24 18:00"),
-        pd.Timestamp("2014-07-07 18:00"),
-        pd.Timestamp("2014-07-28 18:00"),
-        pd.Timestamp("2014-07-17 06:00"),
-        pd.Timestamp("2014-07-26 12:00"),
-        pd.Timestamp("2014-07-23 06:00"),
-        pd.Timestamp("2014-07-11 18:00"),
-        pd.Timestamp("2014-07-18 06:00"),
+        pd.Timestamp("2014-07-07 06:00:00"),
+        pd.Timestamp("2014-07-07 18:00:00"),
+        pd.Timestamp("2014-07-09 06:00:00"),
+        pd.Timestamp("2014-07-10 18:00:00"),
+        pd.Timestamp("2014-07-11 18:00:00"),
+        pd.Timestamp("2014-07-17 06:00:00"),
+        pd.Timestamp("2014-07-18 06:00:00"),
+        pd.Timestamp("2014-07-19 00:00:00"),
+        pd.Timestamp("2014-07-23 06:00:00"),
+        pd.Timestamp("2014-07-24 18:00:00"),
+        pd.Timestamp("2014-07-26 12:00:00"),
+        pd.Timestamp("2014-07-28 18:00:00"),
     ]
 else:
+    ensemble_directory = context.output_dir / "ensemble_output_nc"
     start_dates = [
-        pd.Timestamp("2014-07-11 12:00"),
-        pd.Timestamp("2014-07-02 12:00"),
-        pd.Timestamp("2014-07-24 18:00"),
-        pd.Timestamp("2014-07-23 12:00"),
-        pd.Timestamp("2014-07-18 06:00"),
-        pd.Timestamp("2014-07-14 18:00"),
-        pd.Timestamp("2014-07-23 00:00"),
+        pd.Timestamp('2014-07-11 12:00:00'), 
+        pd.Timestamp('2014-07-24 18:00:00'), 
+        pd.Timestamp('2014-07-18 06:00:00'), 
+        pd.Timestamp('2014-07-14 18:00:00'), 
+        pd.Timestamp('2014-07-23 00:00:00'),
     ]
 
 simulation_time = pd.Timedelta(2, "days")
 
-max_iters = 40
+max_iters = 30
 
 exp_id = "ENNC"
-ensemble_directory = context.output_dir / "ensemble_output_nc"
 run_directory = context.output_dir / exp_id
 
 non_converged_experiments = []
@@ -78,7 +78,7 @@ if __name__ == "__main__":
 
     aoscm = AOSCM(context)
 
-    ifs_input_file = context.data_dir / "ifs" / f"papa_2014-07_era.nc"
+    ifs_input_file = context.data_dir / f"papa_2014-07_era.nc"
     ifs_forcing_start, ifs_forcing_freq, ifs_levels = get_ifs_forcing_info(
         ifs_input_file
     )
@@ -114,7 +114,8 @@ if __name__ == "__main__":
 
         schwarz = SchwarzCoupling(experiment, context)
         schwarz.run(max_iters, stop_at_convergence=True, rel_tol=1e-5)
-        assert schwarz.iter == max_iters
+        if mass_flux:
+            assert schwarz.iter == max_iters
         for iter in range(1, schwarz.iter + 1):
             nonconverged_schwarz_dir = Path(f"{schwarz.run_directory}_{iter}")
             nonconverged_schwarz_dir.rename(start_date_directory / f"iter_{iter}")
